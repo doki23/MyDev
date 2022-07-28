6dd392c3bf80af31f0a142c2b64dc1b9b651763d
doki <tunyum94@gmail.com>
2022-07-28 19:57:40 +0800

    support cpp

---
+++ Dockerfile.cpp
@@ -0,0 +1,37 @@
+FROM ubuntu:22.04
+
+RUN DEBIAN_FRONTEND="noninteractive" apt-get update && apt-get -y install tzdata
+
+RUN apt-get update \
+  && apt-get install -y ssh \
+      build-essential \
+      gcc \
+      g++ \
+      gdb \
+      clang \
+      make \
+      ninja-build \
+      cmake \
+      autoconf \
+      automake \
+      locales-all \
+      dos2unix \
+      rsync \
+      tar \
+      python2.7 \
+  && apt-get clean
+
+RUN ( \
+    echo 'LogLevel DEBUG2'; \
+    echo 'PermitRootLogin yes'; \
+    echo 'PasswordAuthentication yes'; \
+    echo 'Subsystem sftp /usr/lib/openssh/sftp-server'; \
+  ) > /etc/ssh/sshd_config_dev_clion \
+  && mkdir /run/sshd
+
+RUN useradd -m dev \
+  && yes dev | passwd dev
+
+RUN usermod -s /bin/bash dev
+
+CMD ["/usr/sbin/sshd", "-D", "-e", "-f", "/etc/ssh/sshd_config_dev_clion"]
